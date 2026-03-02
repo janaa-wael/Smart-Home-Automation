077ae5319c7dc27fb6d0191228877ee2476b1818
janaa-wael <janawael119@gmail.com>
2026-03-02 15:01:17 +0200

    Adding Thermostat Class Header File

---
+++ Thermostat.h
@@ -0,0 +1,25 @@
+#include <iostream>
+#include "SmartDevice.h"
+using namespace std;
+
+class Thermostat : public SmartDevice
+{
+private:
+    float temperature;
+    typedef enum {
+        HEATING,
+        COOLING,
+        OFF
+    }SwitchingMode;
+    SwitchingMode mode;
+public: 
+    Thermostat();
+    Thermostat(string& name);
+    void setTemperature(float temperature);
+    void setSwitchingMode(SwitchingMode mode);
+    SwitchingMode getSwicthingMode();
+    float readTemperature();
+    virtual void activate();
+    virtual void deactivate();
+
+} ;
